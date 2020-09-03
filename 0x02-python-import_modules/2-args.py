3a5c5cfdd7de2153205c2a45cab256535f9d25af
steffanynaranjov <1792@holbertonschool.con>
2020-09-03 22:17:43 +0000

    delete extra s

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if not argc:
         print("0 arguments.")
     else:
-        print("{} arguments{}:".format(argc, "s" if argc > 1 else ""))
+        print("{} argument{}:".format(argc, "s" if argc > 1 else ""))
         for i, x in enumerate(argv[1:], 1):
             print("{}: {}.".format(i, x))
