c2658c64dce37ae6e28ecc6374b6728e31bf38e8
steffanynaranjov <1792@holbertonschool.con>
2020-09-03 22:14:54 +0000

    delete space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    def main():
     argc = len(argv) - 1
     if not argc:
         print("0 arguments.")
     else:
-        print("{} arguments{}:" .format(argc, "s" if argc > 1 else ""))
+        print("{} arguments{}:".format(argc, "s" if argc > 1 else ""))
         for i, x in enumerate(argv[1:], 1):
             print("{}: {}.".format(i, x))
