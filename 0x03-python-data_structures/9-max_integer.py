913b84e450dda397e52d88d3edf05e1f1da812f1
steffanynaranjov <1792@holbertonschool.con>
2020-09-08 23:37:42 +0000

    maximun num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list is not None and len(my_list) > 0:
-        max = 0
+        max_num = 0
         for num in my_list:
-            if num >= max:
-                max = num
-        return max
+            if num >= max_num:
+                max_num = num
+        return max_num
