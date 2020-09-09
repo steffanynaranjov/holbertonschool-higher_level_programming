776c8444d6d4d676486f1dfb6e0d11a0d30c29c3
steffanynaranjov <1792@holbertonschool.con>
2020-09-09 04:33:03 +0000

    last last try

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,26 +7,32 @@
 */
 int is_palindrome(listint_t **head)
 {
-	int buffer[1000];
-	listint_t *tmp_node = *head;
-	int x, y, z;
+	listint_t *temp = *head;
+	int buff[1000];
+	int i = 0, j = 0, size = 0;
 
-	if (tmp_node == NULL)
+	if (head == NULL)
 		return (1);
-	x = 0;
-	while (tmp_node)
-	{
-		buffer[x] = tmp_node->n;
-		tmp_node = tmp_node->next;
-		x++;
-	}
-	z = (x + 1) / 2;
-	x = x - 1;
 
-	for (y = 0; y < z; y++, x--)
+	if (temp->next == NULL)
+		return (1);
+
+	if (*head == NULL)
+		return (1);
+
+	while (temp != NULL)
 	{
-		if (buffer[y] != buffer[x])
+		buff[i] = temp->n;
+		temp = temp->next;
+		i++;
+	}
+	size = i - 1;
+	while (j <= size)
+	{
+		if (buff[size] != buff[j])
 			return (0);
+		size--;
+		j++;
 	}
 	return (1);
 }
