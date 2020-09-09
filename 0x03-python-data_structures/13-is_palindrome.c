47867dd2794c1f29f2fb38291f9e8986b8a2044b
steffanynaranjov <1792@holbertonschool.con>
2020-09-09 04:41:30 +0000

    hsjd

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,35 @@
 #include "lists.h"
 /**
-* is_palindrome - checks if the list of integers is palindrome
-* @head: pointer to a pointer
+* check_palindrome - recursive function
+* @first: first of list
+* @last: last position of list
+*
+* Return: 0 or 1
+*/
+int check_palindrome(listint_t **first, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+
+	if (check_palindrome(first, last->next) == 1 && (*first)->n == last->n)
+	{
+		*first = (*first)->next;
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: list to check
 *
 * Return: 0 or 1
 */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head;
-	int buff[1000];
-	int i = 0, j = 0, size = 0;
-
-	if (head == NULL)
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
 		return (1);
 
-	if (temp->next == NULL)
-		return (1);
-
-	if (*head == NULL)
-		return (1);
-
-	while (temp != NULL)
-	{
-		buff[i] = temp->n;
-		temp = temp->next;
-		i++;
-	}
-	size = i - 1;
-	while (j <= size)
-	{
-		if (buff[size] != buff[j])
-			return (0);
-		size--;
-		j++;
-	}
-	return (1);
+	return (check_palindrome(head, *head));
 }
