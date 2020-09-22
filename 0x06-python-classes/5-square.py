f721048c284688e075d48ca2188a7547ef30f5d8
steffanynaranjov <1792@holbertonschool.con>
2020-09-22 23:54:13 +0000

    square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This is a class Square defined by size."""
+
+
+class Square:
+    """Inizialitation class"""
+    def __init__(self, size=0):
+        """The initialization method of the square class
+        Args:
+            size (int): Is the type int private attribute
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Private instance attribute getter method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Private instanse property setter."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """The are public of the square class"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #"""
+        if not self.__size:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
