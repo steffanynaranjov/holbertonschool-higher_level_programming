e678769db792c43dbb93ceee5aa2c5d58abd4855
steffanynaranjov <1792@holbertonschool.con>
2020-09-22 23:19:31 +0000

    hhfkf

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,18 +5,18 @@
 class Square:
     """Inizialitation class"""
     def __init__(self, size=0):
-         """The initialization method of the square class
+        """The initialization method of the square class
 
-         Args:
+        Args:
             size (int): Is the type int private attribute
 
         Raises:
-         TypeError: If size is not an integer
-         ValueError: If size is less than 0
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
 
-         """
-         self.__size = size
-         if not isinstance(size, int):
-             raise TypeError("size must be an integer")
-         if size < 0:
-             raise ValueError("size must be >= 0")
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
