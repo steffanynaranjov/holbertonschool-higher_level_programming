06197460ad810fa3b3c942c84664426f0fd00971
steffanynaranjov <1792@holbertonschool.con>
2020-09-22 23:24:03 +0000

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""This is a class Square defined by size."""
+
+
 class Square:
+    """Inizialitation class"""
     def __init__(self, size=0):
+        """The initialization method of the square class
+        Args:
+            size (int): Is the type int private attribute
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
         self.__size = size
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-
     def area(self):
+        """The area public instance method of the square class"""
         return (self.__size * self.__size)
