1245bd2684c9b52ce4d275d4a7032a2e793a2cec
steffanynaranjov <1792@holbertonschool.con>
2020-09-22 22:49:24 +0000

    3.5

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """This is a class Square defined by size."""
 
+
 class Square:
     "Def"
     def __init__(self, size):
         """The initialization method of the square class
 
         Args:
-            size: Is the type int private attribute
+            size (int): Is the type int private attribute
 
         """
         self.__size = size
