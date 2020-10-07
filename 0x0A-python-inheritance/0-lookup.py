faf1bc19b5d308ca1ed6c1ae6eb05f225c233aaa
steffanynaranjov <1792@holbertonschool.con>
2020-10-07 00:09:39 +0000

    fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Return dir of the a object
+"""
+
+
 def lookup(obj):
-    """Returns the list all the attributes and method"""
+    """
+    Returns the list all the attributes and method
+    """
     return (dir(obj))
