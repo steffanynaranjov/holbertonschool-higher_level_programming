89df10e017c6df06513009ac1d7d2e8b417208aa
steffanynaranjov <66284424+steffanynaranjov@users.noreply.github.com>
2020-10-06 19:05:50 -0500

    delete it

--- 0x0A-python-inheritance/101-add_attribute.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def add_attribute(obj, name, value):
-    """add_attribute method"""
-    if hasattr(obj, "__dict__"):
-        setattr(obj, name, value)
-    else:
-        raise TypeError("can't add new attribute")
