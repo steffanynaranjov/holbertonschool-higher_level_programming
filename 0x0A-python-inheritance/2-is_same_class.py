59bc19c5addf9ac5f26b6a2970e370f0d02abf73
steffanynaranjov <1792@holbertonschool.con>
2020-10-07 00:28:28 +0000

    2.0

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+check if the object is an instance or a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if the object is an instance or a class
+    """
+    if (type(obj) == a_class):
+        return True
+    return False
