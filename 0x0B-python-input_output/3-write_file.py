77a5bd039dd2e72db66e4c261c2bd15d6126124b
steffanynaranjov <1792@holbertonschool.con>
2020-10-07 22:24:59 +0000

    fix

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
     Writes a string to a text file (UTF8)
     """
     with open(filename, mode="w", encoding="utf-8") as m_file:
-        char = m_file.char(text)
-    return char
+        char = m_file.write(text)
+        return char
