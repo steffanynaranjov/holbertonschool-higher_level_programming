9168baa73dd4551edc082c967eea4223733e85da
steffanynaranjov <1792@holbertonschool.con>
2020-10-07 00:34:50 +0000

    3.0

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+check if the object is an instance or an instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    check if the object is an instance or an instance
+    """
+    return (isinstance(obj, a_class))
