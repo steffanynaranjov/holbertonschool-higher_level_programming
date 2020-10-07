97934d110309de66df57a39cf8cca5de262ebb4a
steffanynaranjov <1792@holbertonschool.con>
2020-10-07 23:52:44 +0000

    12.1

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Class Student that defines a student
+"""
+
+
+class Student:
+    """
+    Class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Variables and instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return the attribute of a object
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        for x in attrs:
+            if not isinstance(x, str):
+                return self.__dict__
+        m_dict = self.__dict__
+        n_dict = {
+            attr: m_dict for attr in attrs if attr in self.__dict__.keys()
+        }
+        return n_dict
