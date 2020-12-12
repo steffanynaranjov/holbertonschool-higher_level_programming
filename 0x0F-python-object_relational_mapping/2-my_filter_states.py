be71d296bd40cf248642e44961d08d438d105e22
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-12 17:15:30 +0000

    where binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-name that match with arg
+connect the mysql
 """
 
 import sys
@@ -9,17 +9,10 @@ import MySQLdb
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
-    cu = conn.cursor()
+    cur = conn.cursor()
 
-    c = 'SELECT * FROM states WHERE name LIKE BINARY "{}"' \
-        ' ORDER BY states.id ASC;'
-    c = c.format(argv[4])
-    cu.execute(c)
-
-    row = cu.fetchall()
-
-    for r in rows:
-        print(r)
-
-    cu.close()
+    cur.execute("SELECT states.id, name FROM states WHERE BINARY name = '{}';"
+                .format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
+    cur.close()
     conn.close()
