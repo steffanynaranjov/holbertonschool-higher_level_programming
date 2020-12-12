efb4905ecb0d7dcf5c357d41442cf5db4a320016
steffanynaranjov <steffany.naranjo@gmail.com>
2020-12-12 07:01:36 +0000

    mandatory

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+connect the mysql
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cu = conn.cursor()
+
+    cu.execute('SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id;',
+               (argv[4],))
+
+    row = cu.fetchall()
+
+    for r in rows:
+        print(r)
+
+    cu.close()
+    conn.close()
