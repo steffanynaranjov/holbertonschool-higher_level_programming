a20cce51e3c5e3f9cc1bf970719e605f49a9e547
steffanynaranjov <steffany.naranjo@gmail.com>
2021-02-03 01:31:17 +0000

    addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+
+const firstNum = Number(process.argv[2]);
+const secondNum = Number(process.argv[3]);
+
+console.log(add(firstNum, secondNum));
