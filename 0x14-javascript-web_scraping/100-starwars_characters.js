3afc1e15f62b5bed0d4f53033e14d06c143d5d56
steffanynaranjov <steffany.naranjo@gmail.com>
2021-02-10 01:23:04 +0000

    character

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, (error, response, content) => {
+  if (!error) {
+    const people = JSON.parse(content).characters;
+    people.forEach((character) => {
+      request(character, (error, response, content) => {
+        if (!error) {
+          console.log(JSON.parse(content).name);
+        }
+      });
+    });
+  }
+});
