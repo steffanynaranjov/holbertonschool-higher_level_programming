46891831b6ec8a6634a608811896d150a9bec26d
steffanynaranjov <steffany.naranjo@gmail.com>
2021-02-10 01:16:37 +0000

    api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, (error, response, body) => {
+  console.log(JSON.parse(body).title || error);
+});
